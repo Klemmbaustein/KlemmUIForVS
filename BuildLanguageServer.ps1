a54063b911446dedcb0ba8bb79a72e090421b700
Klemmbaustein <jan.paschke@outlook.de>
2026-09-04 18:22:28 +0200

    add native language server

---
+++ BuildLanguageServer.ps1
@@ -0,0 +1,21 @@
+param (
+	[string] $buildConfig = "Debug"
+)
+
+$serverPath = "KlemmUILanguageServer"
+$cmakeOutDir = "out/build/script"
+$serverExecutable = "Resources/KlemmUILanguageServer.exe"
+
+cd ${serverPath}
+cmake -S . -B ${cmakeOutDir}
+cmake --build ${cmakeOutDir} --config ${buildConfig} -j 12
+cd ..
+
+if (test-path ${serverPath}/${cmakeOutDir}/KlemmUILanguageServer.exe)
+{
+	cp ${serverPath}/${cmakeOutDir}/KlemmUILanguageServer.exe ${serverExecutable}
+}
+else
+{
+	cp ${serverPath}/${cmakeOutDir}/${buildConfig}/KlemmUILanguageServer.exe ${serverExecutable}
+}
