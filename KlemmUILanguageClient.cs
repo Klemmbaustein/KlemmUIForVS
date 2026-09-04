16c4b9276a6764afc0e4f6cff30591b5788e8518
Klemmbaustein <jan.paschke@outlook.de>
2026-09-04 18:00:33 +0200

    Add project files.

---
+++ KlemmUILanguageClient.cs
@@ -0,0 +1,134 @@
+using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using StreamJsonRpc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Threading;
+using Task = System.Threading.Tasks.Task;
+using System.ComponentModel.Composition;
+
+namespace KlemmUI.LanguageServer
+{
+	[ContentType("kui")]
+	[Export(typeof(ILanguageClient))]
+	[RunOnContext(RunningContext.RunOnHost)]
+	public class KlemmUILanguageClient : ILanguageClient, ILanguageClientCustomMessage2
+	{
+		public KlemmUILanguageClient()
+		{
+			Instance = this;
+		}
+
+		internal static KlemmUILanguageClient? Instance
+		{
+			get;
+			set;
+		}
+
+		internal JsonRpc? Rpc
+		{
+			get;
+			set;
+		}
+
+		public event AsyncEventHandler<EventArgs>? StartAsync;
+		public event AsyncEventHandler<EventArgs>? StopAsync;
+
+		public string Name => "KlemmUI LSP";
+
+		public IEnumerable<string> ConfigurationSections
+		{
+			get
+			{
+				yield return "kui";
+			}
+		}
+
+		public object? InitializationOptions => null;
+
+		public IEnumerable<string>? FilesToWatch => null;
+
+		public object? MiddleLayer
+		{
+			get;
+			set;
+		}
+
+		public object? CustomMessageTarget => null;
+
+		public bool ShowNotificationOnInitializeFailed => true;
+
+		public async Task<Connection?> ActivateAsync(CancellationToken token)
+		{
+			await Task.Yield();
+
+			string executable = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Resources", "KlemmUILanguageServer.exe");
+
+			Process process = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = executable,
+					WorkingDirectory = Path.GetDirectoryName(executable),
+					RedirectStandardInput = true,
+					RedirectStandardOutput = true,
+					UseShellExecute = false,
+					CreateNoWindow = true,
+				}
+			};
+
+			if (process.Start())
+			{
+				return new Connection(process.StandardOutput.BaseStream, process.StandardInput.BaseStream);
+			}
+			return null;
+		}
+
+		public async Task OnLoadedAsync()
+		{
+			if (StartAsync != null)
+			{
+				await StartAsync.InvokeAsync(this, EventArgs.Empty);
+			}
+		}
+
+		public async Task StopServerAsync()
+		{
+			if (StopAsync != null)
+			{
+				await StopAsync.InvokeAsync(this, EventArgs.Empty);
+			}
+		}
+
+		public Task OnServerInitializedAsync()
+		{
+			return Task.CompletedTask;
+		}
+
+		public Task AttachForCustomMessageAsync(JsonRpc rpc)
+		{
+			Rpc = rpc;
+
+			return Task.CompletedTask;
+		}
+
+		public Task<InitializationFailureContext?> OnServerInitializeFailedAsync(ILanguageClientInitializationInfo initializationState)
+		{
+			string message = "KlemmUI language server initialization failed.";
+			string exception = initializationState.InitializationException?.ToString() ?? string.Empty;
+			message = $"{message}\n {exception}";
+
+			InitializationFailureContext? failureContext = new()
+			{
+				FailureMessage = message,
+			};
+
+			return Task.FromResult(failureContext);
+		}
+	}
+}
---
+++ KuiLanguageTypeDefinition.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace KlemmUI.LanguageServer
+{
+#pragma warning disable 649
+	public class KuiLanguageTypeDefinition
+	{
+		[Export]
+		[Name("kui")]
+		[BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+		internal static ContentTypeDefinition? KuiContentTypeDefinition;
+
+
+		[Export]
+		[FileExtension(".kui")]
+		[ContentType("kui")]
+		internal static FileExtensionToContentTypeDefinition? KuiFileExtensionDefinition;
+	}
+#pragma warning restore 649
+}
